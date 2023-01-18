3ac38f4e38e4344e6158bb000eca40c89d09fcc8
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-17 16:05:25 -0800

    create HTML, README, and Javascript files.
    Co-authored-by: Erin Timlin <erintimlin@gmail.com>.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//User Logic
+
+window.onload = function() {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault()
+    const someFavorites = document.getElementById("favoriteThing").value;
+
+
+
+    // document.querySelector("id#favoriteThingOne").innerText = "favoriteThingOne";
+  // const favoriteThingOne
+  // const favoriteThingTwo
+  // const favoriteThingThree
+
+
+  // document.querySelector("div#story").removeAttribute("class");
+  // };
+};
+};
