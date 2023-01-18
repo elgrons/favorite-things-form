7150b2277cbc35dbfdd368a3355238ee65e253aa
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-17 16:25:44 -0800

    update javascript with WIP to publish results via array.
    Co-Authored-By: Erin Timlin <erintimlin@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,14 @@ window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
     event.preventDefault()
-    const someFavorites = document.getElementById("favoriteThing").value;
+    const oneFavorite = document.getElementById("favoriteThingOne").value;
+    const twoFavorite = document.getElementById("favoriteThingTwo").value;
+    const threeFavorite = document.getElementById("favoriteThingThree").value;
+    const tripleFaveArray = ["oneFavorite", "twoFavorite", "threeFavorite"];
 
-
-
-    // document.querySelector("id#favoriteThingOne").innerText = "favoriteThingOne";
-  // const favoriteThingOne
-  // const favoriteThingTwo
-  // const favoriteThingThree
-
-
-  // document.querySelector("div#story").removeAttribute("class");
-  // };
+  const seeResults = document.createElement("p");
+  seeResults.append("Your favorite things are:");
+  seeResults.append(tripleFaveArray);
+  document.querySelector("body").append(seeResults)
 };
 };
